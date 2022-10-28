fdb007929f2c4ba802313cbc6b073080189713c1
Daniel Klein <daniel.klein@develit.io>
2022-10-28 10:11:24 +0200

    fix: remove generated code

--- lib/pyramid_funnel_levels.dart
+++ lib/pyramid_funnel_levels.dart
@@ -1,7 +1,3 @@
 library pyramid_funnel_levels;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './models/level_tree/level_tree.dart';
